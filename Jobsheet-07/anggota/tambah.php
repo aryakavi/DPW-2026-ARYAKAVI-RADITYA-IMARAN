9efe58ec7ce2531222fdb841d7f5d6aae74e3eda
aryakavi <aryakaviimaran2007@gmail.com>
2026-09-23 21:16:36 +0700

    Jobsheet 7 - BAB 4 - Merubah file tambah.php di kedua folder (buku dana nggota) untuk blok tampil flash diatas form

--- Jobsheet-07/anggota/tambah.php
+++ Jobsheet-07/anggota/tambah.php
@@ -1,10 +1,18 @@
 <?php
 $page_title = "Tambah Anggota";
 include __DIR__ . '/../includes/header.php';
+
+$flash = $_SESSION['flash'] ?? null;
+unset($_SESSION['flash']);
 ?>
         <section>
             <h2>Tambah Anggota</h2>
-            <form id="form-tambah">
+
+            <?php if ($flash): ?>
+                <p class="flash flash-<?php echo $flash['type']; ?>"><?php echo $flash['pesan']; ?></p>
+            <?php endif; ?>
+
+            <form id="form-tambah" method="post" action="proses_tambah.php">
                 <p>
                     <label for="nama">Nama</label><br>
                     <input type="text" id="nama" name="nama" required>
--- Jobsheet-07/buku/tambah.php
+++ Jobsheet-07/buku/tambah.php
@@ -1,10 +1,17 @@
 <?php
 $page_title = "Tambah Buku";
 include __DIR__ . '/../includes/header.php';
+$flash = $_SESSION['flash'] ?? null;
+unset($_SESSION['flash']);
 ?>
         <section>
             <h2>Tambah Buku</h2>
-            <form id="form-tambah">
+
+            <?php if ($flash): ?>
+                <p class="flash flash-<?php echo $flash['type']; ?>"><?php echo $flash['pesan']; ?></p>
+            <?php endif; ?>
+            
+            <form id="form-tambah" method="post" action="proses_tambah.php">
                 <p>
                     <label for="judul">Judul</label><br>
                     <input type="text" id="judul" name="judul" required>
